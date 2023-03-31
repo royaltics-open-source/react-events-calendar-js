103dd507fb699d6314a325b9f3fece926ff66db9
Djasc <djasc.dev@gmail.com>
2023-03-31 08:55:51 -0500

    Fix bugs

--- src/components/calendarBody.tsx
+++ src/components/calendarBody.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { buildDaysWeekIndex, buildMonthCalendar } from '../utils/Builder';
+import {  buildMonthCalendar } from '../utils/Builder';
 import CalendarBodyItem from './calendarBodyItem';
 import { CalendarBodyProps } from '../types/CalendarTypes';
 
@@ -7,7 +7,6 @@ const CalendarBody = ({ options, onClick, onMouseOver, onMouseOut }: CalendarBod
 
   //let stopCount = dayInMonth(options.year, options.month);
   let buildDays = buildMonthCalendar(options);
-  let dayOfWeeks = buildDaysWeekIndex(buildDays.startDayWeek);
 
   return (
     <div className='calendar-body'>
